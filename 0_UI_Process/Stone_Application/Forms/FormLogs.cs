0969de7650d549ccdb93a1feb6490cfda75a2885
QUOCHUY <quochuylv304@gmail.com>
2026-05-16 12:28:20 +0700

    fix: pdf export

--- 0_UI_Process/Stone_Application/Forms/FormLogs.cs
+++ 0_UI_Process/Stone_Application/Forms/FormLogs.cs
@@ -10,6 +10,8 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using DocumentFormat.OpenXml.Drawing;
 using Stone_Application.Event;
+using Stone_Application.PDFExport;
+using Stone_Application;
 
 namespace Stone_Application.Forms
 {
@@ -66,6 +68,39 @@ namespace Stone_Application.Forms
                     instance.textBoxLogging.TextLength;
 
                 instance.textBoxLogging.ScrollToCaret();
+            }));
+        }
+
+        private void buttonExportPDFClick(object sender, EventArgs e)
+        {
+            string startTime = Common.repositoryInstance.getStartTime();
+            string currentTime = Common.repositoryInstance.getLatestTime();
+
+            if (startTime == null || currentTime == null)
+            {
+                MessageBox.Show("No data to export.", "Export PDF", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+
+            string outputFile = Config.outputPath + currentTime + ".docx";
+
+            PDFExportcs.ExportFile(
+                outputFilePath: outputFile,
+                startTime: startTime,
+                totalTime: currentTime,
+                loadcellRecord: Common.repositoryInstance.getTotal().measuredWeight,
+                realRecord: 0,
+                deviation: 0,
+                isOk: false,
+                perctMisang: Common.repositoryInstance.getTotal().deltaPerctMiSang,
+                perct1x2: Common.repositoryInstance.getTotal().deltaPerct1x2,
+                perct2x4: Common.repositoryInstance.getTotal().deltaPerct2x4,
+                perct4x6: Common.repositoryInstance.getTotal().deltaPerct4x6
+            );
+
+            this.Invoke(new Action(() =>
+            {
+                MessageBox.Show("Export successful to" + outputFile, "Export PDF", MessageBoxButtons.OK, MessageBoxIcon.Information);
             }));
         }
     }
