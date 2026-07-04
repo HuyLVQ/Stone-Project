a9ec812fc191077f8e7e8a814269099c40fb76f2
QUOCHUY <quochuylv304@gmail.com>
2026-07-04 22:25:32 +0700

    add: start-stop button in FormHome and user input field in FormLog

--- 0_UI_Process/Stone_Application/Forms/FormHome.Designer.cs
+++ 0_UI_Process/Stone_Application/Forms/FormHome.Designer.cs
@@ -29,30 +29,63 @@ namespace Stone_Application.Forms
         private void InitializeComponent()
         {
             this.m_groupBoxPicture = new System.Windows.Forms.GroupBox();
+            this.stopButton = new System.Windows.Forms.Button();
+            this.startButton = new System.Windows.Forms.Button();
             this.pictureBox = new System.Windows.Forms.PictureBox();
             this.m_groupBoxPicture.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
             this.SuspendLayout();
             // 
-            // groupBoxPicture
+            // m_groupBoxPicture
             // 
+            this.m_groupBoxPicture.Controls.Add(this.stopButton);
+            this.m_groupBoxPicture.Controls.Add(this.startButton);
             this.m_groupBoxPicture.Controls.Add(this.pictureBox);
             this.m_groupBoxPicture.Dock = System.Windows.Forms.DockStyle.Fill;
             this.m_groupBoxPicture.Font = new System.Drawing.Font("Nirmala UI", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.m_groupBoxPicture.Location = new System.Drawing.Point(12, 12);
             this.m_groupBoxPicture.Margin = new System.Windows.Forms.Padding(12);
-            this.m_groupBoxPicture.Name = "groupBoxPicture";
+            this.m_groupBoxPicture.Name = "m_groupBoxPicture";
             this.m_groupBoxPicture.Padding = new System.Windows.Forms.Padding(16);
             this.m_groupBoxPicture.Size = new System.Drawing.Size(1056, 776);
             this.m_groupBoxPicture.TabIndex = 0;
             this.m_groupBoxPicture.TabStop = false;
             this.m_groupBoxPicture.Text = "Video Stream";
             // 
+            // stopButton
+            // 
+            this.stopButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.stopButton.Enabled = false;
+            this.stopButton.Font = new System.Drawing.Font("Nirmala UI", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.stopButton.ForeColor = System.Drawing.SystemColors.ActiveBorder;
+            this.stopButton.Location = new System.Drawing.Point(194, 723);
+            this.stopButton.Name = "stopButton";
+            this.stopButton.Size = new System.Drawing.Size(155, 37);
+            this.stopButton.TabIndex = 2;
+            this.stopButton.Text = "Stop";
+            this.stopButton.UseVisualStyleBackColor = true;
+            // 
+            // startButton
+            // 
+            this.startButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.startButton.Enabled = false;
+            this.startButton.Font = new System.Drawing.Font("Nirmala UI", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startButton.ForeColor = System.Drawing.SystemColors.ActiveBorder;
+            this.startButton.Location = new System.Drawing.Point(24, 723);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(155, 37);
+            this.startButton.TabIndex = 1;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            // 
             // pictureBox
             // 
-            this.pictureBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox.Location = new System.Drawing.Point(24, 58);
             this.pictureBox.Name = "pictureBox";
-            this.pictureBox.Size = new System.Drawing.Size(1024, 714);
+            this.pictureBox.Size = new System.Drawing.Size(1001, 649);
             this.pictureBox.TabIndex = 0;
             this.pictureBox.TabStop = false;
             // 
@@ -63,8 +96,8 @@ namespace Stone_Application.Forms
             this.ClientSize = new System.Drawing.Size(1080, 800);
             this.Controls.Add(this.m_groupBoxPicture);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Padding = new System.Windows.Forms.Padding(12);
             this.Name = "FormHome";
+            this.Padding = new System.Windows.Forms.Padding(12);
             this.Text = "FormHome";
             this.m_groupBoxPicture.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
@@ -76,5 +109,7 @@ namespace Stone_Application.Forms
 
         private System.Windows.Forms.GroupBox m_groupBoxPicture;
         public System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Button stopButton;
     }
 }
--- 0_UI_Process/Stone_Application/Forms/FormLogs.Designer.cs
+++ 0_UI_Process/Stone_Application/Forms/FormLogs.Designer.cs
@@ -29,31 +29,57 @@ namespace Stone_Application.Forms
         private void InitializeComponent()
         {
             this.m_groupBoxLogging = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.userInputTextBox = new System.Windows.Forms.TextBox();
             this.m_buttonExportPDF = new System.Windows.Forms.Button();
             this.textBoxLogging = new System.Windows.Forms.RichTextBox();
             this.m_groupBoxLogging.SuspendLayout();
             this.SuspendLayout();
             // 
-            // groupBoxLogging
+            // m_groupBoxLogging
             // 
+            this.m_groupBoxLogging.Controls.Add(this.label1);
+            this.m_groupBoxLogging.Controls.Add(this.userInputTextBox);
             this.m_groupBoxLogging.Controls.Add(this.m_buttonExportPDF);
             this.m_groupBoxLogging.Controls.Add(this.textBoxLogging);
             this.m_groupBoxLogging.Dock = System.Windows.Forms.DockStyle.Fill;
             this.m_groupBoxLogging.Font = new System.Drawing.Font("Nirmala UI", 19.8F, System.Drawing.FontStyle.Bold);
             this.m_groupBoxLogging.Location = new System.Drawing.Point(12, 12);
             this.m_groupBoxLogging.Margin = new System.Windows.Forms.Padding(12);
-            this.m_groupBoxLogging.Name = "groupBoxLogging";
+            this.m_groupBoxLogging.Name = "m_groupBoxLogging";
             this.m_groupBoxLogging.Size = new System.Drawing.Size(1056, 776);
             this.m_groupBoxLogging.TabIndex = 0;
             this.m_groupBoxLogging.TabStop = false;
             this.m_groupBoxLogging.Text = "Logging";
             // 
-            // buttonExportPDF
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Nirmala UI", 7.8F, System.Drawing.FontStyle.Bold);
+            this.label1.Location = new System.Drawing.Point(55, 734);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(141, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Actual Weight (in kg)";
+            // 
+            // userInputTextBox
+            // 
+            this.userInputTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.userInputTextBox.Font = new System.Drawing.Font("Nirmala UI", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.userInputTextBox.Location = new System.Drawing.Point(213, 725);
+            this.userInputTextBox.Name = "userInputTextBox";
+            this.userInputTextBox.Size = new System.Drawing.Size(641, 31);
+            this.userInputTextBox.TabIndex = 2;
+            // 
+            // m_buttonExportPDF
             // 
             this.m_buttonExportPDF.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.m_buttonExportPDF.Enabled = false;
             this.m_buttonExportPDF.Font = new System.Drawing.Font("Nirmala UI", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.m_buttonExportPDF.ForeColor = System.Drawing.SystemColors.ActiveBorder;
             this.m_buttonExportPDF.Location = new System.Drawing.Point(870, 723);
-            this.m_buttonExportPDF.Name = "buttonExportPDF";
+            this.m_buttonExportPDF.Name = "m_buttonExportPDF";
             this.m_buttonExportPDF.Size = new System.Drawing.Size(155, 37);
             this.m_buttonExportPDF.TabIndex = 1;
             this.m_buttonExportPDF.Text = "Export PDF";
@@ -78,10 +104,11 @@ namespace Stone_Application.Forms
             this.ClientSize = new System.Drawing.Size(1080, 800);
             this.Controls.Add(this.m_groupBoxLogging);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Padding = new System.Windows.Forms.Padding(12);
             this.Name = "FormLogs";
+            this.Padding = new System.Windows.Forms.Padding(12);
             this.Text = "FormLogs";
             this.m_groupBoxLogging.ResumeLayout(false);
+            this.m_groupBoxLogging.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -91,5 +118,7 @@ namespace Stone_Application.Forms
         private System.Windows.Forms.GroupBox m_groupBoxLogging;
         public System.Windows.Forms.RichTextBox textBoxLogging;
         private System.Windows.Forms.Button m_buttonExportPDF;
+        private System.Windows.Forms.TextBox userInputTextBox;
+        private System.Windows.Forms.Label label1;
     }
 }
