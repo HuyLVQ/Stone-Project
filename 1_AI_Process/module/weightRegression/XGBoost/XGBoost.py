eef19e0d9489879bf005b994d3c3cbb979fa792f
QUOCHUY_LAB_PC <quochuylv304@gmail.com>
2026-07-27 11:22:26 +0700

    Update model

--- 1_AI_Process/module/weightRegression/XGBoost/XGBoost.py
+++ 1_AI_Process/module/weightRegression/XGBoost/XGBoost.py
@@ -5,7 +5,7 @@ def predictXGBoost(p_XNew):
     if np.all(np.asarray(p_XNew)==0):
         return 0
     else:
-        loadedModel = joblib.load(Path(__file__).resolve().parent / "xgboost.pkl")
+        loadedModel = joblib.load(Path(__file__).resolve().parent / "xgboost_v2.pkl")
 
         predictions = loadedModel.predict(p_XNew)
         return predictions[0]
