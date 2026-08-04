6ea05f4eacb1401334830488bbaea09fe4747b2f
kiennguyen163 <kien.nguyen20904@hcmut.edu.vn>
2026-08-04 10:02:39 +0700

    Update model 04/08/2026

--- 1_AI_Process/module/weightRegression/XGBoost/XGBoost.py
+++ 1_AI_Process/module/weightRegression/XGBoost/XGBoost.py
@@ -2,6 +2,6 @@ import joblib
 from pathlib import Path
 import numpy as np
 def predictXGBoost(p_XNew):
-    loadedModel = joblib.load(Path(__file__).resolve().parent / "xgboost_v3.pkl")
+    loadedModel = joblib.load(Path(__file__).resolve().parent / "xgboost_v4.pkl")
     predictions = loadedModel.predict(p_XNew)
     return predictions[0]
